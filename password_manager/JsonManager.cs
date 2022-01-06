42418b1d2158d81011d8be6b0c05fce9ecd35b0e
giovannialbertos <97249640+giovannialbertos@users.noreply.github.com>
2022-01-06 20:02:56 +0100

    Add files via upload

---
+++ password_manager/JsonManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace password_manager
+{
+    static class JsonManager
+    {
+
+        public static void JsonSerialize(List<Entry> listOfEntries)
+        {
+
+            string filename = "myDatabase.json";
+            string jsonstring = JsonSerializer.Serialize(listOfEntries);
+            File.WriteAllText(filename, jsonstring);
+        }
+
+        public static List<Entry> JsonDeserialize() 
+        {
+            string fileName = "myDatabase.json";
+
+            if (new FileInfo(fileName).Length == 0)
+            {
+                // file is empty
+                List<Entry> ListOfEntries=new List<Entry>();
+                return ListOfEntries;
+            }
+            else
+            {
+                string jsonString = File.ReadAllText(fileName);
+                List<Entry> ListOfEntries = JsonSerializer.Deserialize<List<Entry>>(jsonString);
+                return ListOfEntries;
+            }
+            
+
+        }
+
+        public static void ShowDatabase(List<Entry> listOfEntries)
+        {
+            if (!listOfEntries.Any())
+            {
+                Console.WriteLine("The database appears to be empty, try adding some entries");
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                Console.Clear();
+                return;
+
+            }
+
+            for (int i = 0; i < listOfEntries.Count; i++)
+            {
+                Console.WriteLine($"Site:     {listOfEntries[i].Site}");
+                Console.WriteLine($"Username: {listOfEntries[i].Username}");
+                Console.WriteLine($"Password: {listOfEntries[i].Password}");
+                Console.WriteLine($"---------------------------------------");
+                
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+
+
+        }
+
+        public static void ShowEntry(Entry entry)
+        {
+           
+                Console.WriteLine($"Site:     {entry.Site}");
+                Console.WriteLine($"Username: {entry.Username}");
+                Console.WriteLine($"Password: {entry.Password}");
+
+
+        }
+
+
+
+    }
+}
---
+++ password_manager/RC4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace password_manager
+{
+    static class RC4
+    {
+        public static string Encrypt(string text, string key)
+        {
+            StringBuilder result = new StringBuilder();
+            int temp_variable, y, j = 0;
+            int[] S = new int[256];
+
+            //initialization of S
+            for (int i = 0; i < 256; i++)
+            {
+                S[i] = i;
+            }
+
+
+            //KSA algorithm
+            //this scrambles the array S using the key 
+            for (int i = 0; i < 256; i++)
+            {
+                j = (key[i % key.Length] + S[i] + j) % 256;
+                temp_variable = S[i];
+                S[i] = S[j];
+                S[j] = temp_variable;
+            }
+
+            j = 0;
+            //PRGA(Pseudo Random Generation Algorithm)
+            for (int i = 0; i < text.Length; i++)
+            {
+                y = (i + 1) % 256;
+                j = (S[y] + j) % 256;
+                temp_variable = S[y];
+                S[y] = S[j];
+                S[j] = temp_variable;
+
+                result.Append((char)(text[i] ^ S[(S[y] + S[j]) % 256]));
+                //Console.WriteLine(result);
+            }
+            return result.ToString();
+
+
+
+
+        }
+
+        public static List<Entry> ComputeDatabase(List<Entry> listOfEntries, string masterPassword)
+        {
+            for (int i = 0; i < listOfEntries.Count; i++)
+            {
+                listOfEntries[i].Site = RC4.Encrypt(listOfEntries[i].Site, masterPassword);
+                listOfEntries[i].Username = RC4.Encrypt(listOfEntries[i].Username, masterPassword);
+                listOfEntries[i].Password = RC4.Encrypt(listOfEntries[i].Password, masterPassword);
+
+            }
+
+            return listOfEntries;
+
+        }
+
+    }
+}
